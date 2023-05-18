a66eeee2ee13b49dedec7398f36c57c8de6e819d
Paul Lang <paul-lang@live.at>
2023-05-18 23:40:08 +0200

    Add first variation of algorithm
    Discripton for this variation:
    - ineffective/O(n^2)
    
    Does not handle edge cases so far. Like for example some elements in the list are not integers.

---
+++ firstVariant.py
@@ -0,0 +1,19 @@
+def maxSum(inputArray):
+    left = 0
+    right = 0
+    maxSum = float('-inf')
+    length = len(inputArray)
+
+    while(left < length):
+
+        right = left
+        currentSum = 0
+        left += 1
+
+        while(right < length):
+            currentSum += inputArray[right]
+            if currentSum > maxSum:
+                maxSum = currentSum
+            right += 1
+
+    return maxSum
---
+++ index.py
@@ -0,0 +1,7 @@
+import firstVariant
+
+array = [1, -2, 7, -4, 4, 2, -3, 6, 2]
+
+maxSum = firstVariant.maxSum(array)
+
+print(maxSum)
