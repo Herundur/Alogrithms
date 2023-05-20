d519dc8ed112551ba0266e70fd9501b2c44b2de9
Paul Lang <paul-lang@live.at>
2023-05-21 01:31:21 +0200

    Add bubble sort
    efficiency: O(n^2)

---
+++ bubbleSort.py
@@ -0,0 +1,20 @@
+def bubbleSort(array):
+    changes = -1
+    while(changes != 0):
+        i = 0
+        currentChanges = 0
+        while(i < len(array)):
+            if(i + 1 >= len(array)):
+                break
+            if(array[i] > array[i + 1]):
+                largerElement = array[i]
+                smallerElement = array[i + 1]
+                array[i + 1] = largerElement
+                array[i] = smallerElement
+                currentChanges += 1
+            i += 1
+        changes = currentChanges
+    return array
+
+
+#
---
+++ index.py
@@ -0,0 +1,7 @@
+import bubbleSort
+
+array = [1, 0, 4, 2, 5, 6, 1, 12]
+print("Unsorted Array: ",array)
+bubbleSort.bubbleSort(array)
+print("Sorted Array: ",array)
+
