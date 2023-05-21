ae1744ce40a477bafe09305ae58d20e3c318f051
Paul Lang <paul-lang@live.at>
2023-05-21 19:16:19 +0200

    Add binary search
    time complexity: O(log n)
    
    restructure repository

---
+++ inde2x.py
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+import firstVariant
+import secondVariant
+
+array = [1, -2, 3, -4, 4, 2, -3, 6, 2]
+
+maxSumFirstVariant = firstVariant.maxSumFirstVariant(array)
+maxSumSecondVariant = secondVariant.maxSumSecondVariant(array)
+
+print(maxSumFirstVariant)
+print(maxSumSecondVariant)
+=======
+import bubbleSort
+import selectionSort
+
+array = [1, 0, 4, 2, 5, 6, 1, 12]
+print("Unsorted Array: ",array)
+#bubbleSort.bubbleSort(array)
+selectionSort.selectionSort(array)
+print("Sorted Array: ",array)
+
+>>>>>>> master-holder
