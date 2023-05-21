390eee5452b77957c34dd0e5747eb0955926529a
Paul Lang <paul-lang@live.at>
2023-05-21 14:19:05 +0200

    Add selection sort
    efficiency: O(n^2)

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import bubbleSort
+import selectionSort
 
 array = [1, 0, 4, 2, 5, 6, 1, 12]
 print("Unsorted Array: ",array)
-bubbleSort.bubbleSort(array)
+#bubbleSort.bubbleSort(array)
+selectionSort.selectionSort(array)
 print("Sorted Array: ",array)
 
